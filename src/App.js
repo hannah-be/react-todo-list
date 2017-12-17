0eaf60cb99d85d8dedc7d4834abdf5197116383e
Hannah Bourke <hannah25@gmail.com>
2017-12-18 10:00:01 +1100

    Use data to render todo items rather than hard coding

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDoItem from './components/ToDoItem'
 
+// Data (model)
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: false },
+  { description: 'Third', completed: false },
+  { description: 'Fourth', completed: true }
+]
+
+
+// UI (view)
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <ToDoItem description="First" completed={true}></ToDoItem>
-      <ToDoItem description="Second"></ToDoItem>
-      <ToDoItem description="Third"></ToDoItem>
+      {
+        items.map((item, index) => (
+          <ToDoItem 
+            key={ index }
+            description={ item.description }
+            completed={ item.completed }
+          />
+        ))
+      }
       </div>
     );
   }
