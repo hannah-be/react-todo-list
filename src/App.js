ba5574f06a2a415f08e3c401cd165d46079f5abb
Hannah Bourke <hannah25@gmail.com>
2017-12-18 11:38:39 +1100

    Add toggle method

--- src/App.js
+++ src/App.js
@@ -2,26 +2,105 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDoItem from './components/ToDoItem'
 
-// Data (model)
-const items = [
-  { description: 'First', completed: true },
-  { description: 'Second', completed: false },
-  { description: 'Third', completed: false },
-  { description: 'Fourth', completed: true }
-]
 
 
-// UI (view)
+// UI (view / controller)
 class App extends Component {
+  // Data (model)
+  state = {
+    items: [
+      { description: 'First', completed: true },
+      { description: 'Second', completed: false },
+      { description: 'Third', completed: false },
+      { description: 'Fourth', completed: true }
+    ]
+
+  }
+
+  // onToggleItemAtIndex = (index) => {
+  //   this.setState((prevState) => {
+  //     // Get current items
+  //     const items = prevState.items
+  //     // Find the item with the particular index
+  //     const item = items[index]
+  //     // Toggle completed status
+  //     item.completed = !item.completed
+  //     // Regurn the changes we have
+  //     return {
+  //       items: items
+  //     }
+  //   })
+  // }
+
+  onToggleItemAtIndex = (index) => {
+    this.setState((prevState) => {
+      // Get current items
+      const beforeItems = prevState.items
+      const afterItems = beforeItems.map((item, itemIndex) => {
+        // WWhen the index of the item that is being passed, matches the item we're mapping through
+        if (itemIndex === index) {
+          // // Create a new copy of that item
+          // const copy = Object.assign(
+          //   {}, // New blank object
+          //   item, // Copy all properties (key value pairs) from existing item
+          //   { completed: !item.completed } // Override the completed value to be the opposite
+          // )
+          // Modern syntax version:
+          return { 
+            ...item, 
+            completed: !item.completed 
+          }
+        // No changes to make to this particular item
+        } else {
+          return item
+        }
+      })
+
+      // Regurn the changes we have
+      return {
+        items: afterItems
+      }
+    })
+  }
+
+
   render() {
+    const items = this.state.items
+
+    const total = items.length 
+
+    let totalCompleted = 0
+    let totalIncomplete = 0
+    items.forEach((item) => {
+      if (item.completed) {
+        totalCompleted += 1
+      } else {
+        totalIncomplete += 1
+      }
+    })
+
     return (
       <div className="App">
+      <dl>
+        <dt>Total completed</dt>
+        <dd>{totalCompleted}</dd>
+        <dt>Total incomplete</dt>
+        <dd>{totalIncomplete}</dd>
+        <dt>Total</dt>
+        <dd>{total}</dd>
+        </dl>
       {
         items.map((item, index) => (
           <ToDoItem 
             key={ index }
             description={ item.description }
             completed={ item.completed }
+            onToggleCompleted={
+              () => {
+                console.log('Todoitem onToggleCompleted received', index)
+                this.onToggleItemAtIndex(index)
+              }
+            }
           />
         ))
       }
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,11 +3,21 @@ import React from 'react'
 const ToDoItem = ({
  description,
  completed = false,
- onClick
+ onToggleCompleted
 }) => (
   <label>
-    <input type='checkbox' checked={ completed }/>
-    {description}
+    <input 
+    type='checkbox' 
+    checked={ completed }
+    onChange={ 
+      (event) => { // Event listener - function that's called by the browser whenever user interaction happens
+        console.log('Clicked checkbox ', description)
+        // When user clicks the checkbox, carry out this function:
+        onToggleCompleted()
+      }
+     } // inform when input changes it's status (state)
+    />
+    { description }
     </label>
 )
 
